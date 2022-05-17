5bfbbd3fa2d5ba065eacea8a3ead3ea24c9d53b3
Brad Sickles <brad.sickles@gmail.com>
2022-05-17 09:51:05 -0400

    Configuring access and error logs to print to stdout/stderr

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -9,5 +9,7 @@ bind = os.getenv('BINDING', f'0.0.0.0:{port}')
 workers = int(os.getenv('WEB_CONCURRENCY', multiprocessing.cpu_count() * 2))
 threads = int(os.getenv('PYTHON_MAX_THREADS', 1))
 
-# Send logs to stdout
-capture_output = True
+# Send access and error logs to stdout
+accesslog = '-'
+errorlog = '-'
+capture_output = False
